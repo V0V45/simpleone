b3d9b75959ddd9a610386248e2f7c96ecab14c6f
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-25 00:14:02 +0300

    Made it universal

--- src/components/search-field/search-field.jsx
+++ src/components/search-field/search-field.jsx
@@ -1,13 +1,15 @@
 import classes from "./search-field.module.css";
 import searchIcon from "../../assets/icons/search-icon.svg";
+import filterIcon from "../../assets/icons/filter-icon.svg";
 
-export default function SearchField({className}) {
+export default function SearchField({className, icon, placeholder}) {
 
     return (
         <div className={classes.container}>
-            <input className={`${classes.searchField} ${className}`} type="text" placeholder="Поиск" />
+            <input className={`${classes.searchField} ${className}`} type="text" placeholder={placeholder} />
             <button className={classes.button}>
-                <img src={searchIcon} alt="" />
+                {icon === "search" && <img src={searchIcon} alt="" />}
+                {icon === "filter" && <img src={filterIcon} alt="" />}
             </button>
         </div>
     );
