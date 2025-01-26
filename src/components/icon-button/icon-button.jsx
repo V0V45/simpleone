519ad6de700b4109bad40120f8acfebc059f76e4
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-26 23:51:35 +0300

    Added calendar icon

--- src/components/icon-button/icon-button.jsx
+++ src/components/icon-button/icon-button.jsx
@@ -3,6 +3,7 @@ import settingsIcon from "../../assets/icons/settings-icon.svg";
 import unpinIcon from "../../assets/icons/unpin-icon.svg";
 import searchIcon from "../../assets/icons/search-icon-black.svg";
 import addIcon from "../../assets/icons/add-icon.svg";
+import calendarIcon from "../../assets/icons/calendar-icon.svg";
 
 export default function IconButton({className, icon, onClick}) {
 
@@ -12,6 +13,7 @@ export default function IconButton({className, icon, onClick}) {
             {icon === "unpin" && <img src={unpinIcon} alt="" />}
             {icon === "search" && <img src={searchIcon} alt="" />}
             {icon === "add" && <img src={addIcon} alt="" />}
+            {icon === "calendar" && <img src={calendarIcon} alt="" />}
         </button>
     );
 }
