8dd33380e3e1300c5bcc0d88a89b14569854c059
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-29 00:02:40 +0300

    Realized removeButton option

--- src/components/textfield-objective/textfield-small/textfield-small.jsx
+++ src/components/textfield-objective/textfield-small/textfield-small.jsx
@@ -1,9 +1,12 @@
 import classes from "./textfield-small.module.css";
+import removeIcon from "../../../assets/icons/close-x-icon.svg";
 import { useState, useRef, useEffect } from "react";
 
-export default function TextFieldSmall({ value, onChange, className }) {
+export default function TextFieldSmall({ value, onChange, className, isRemoveButtonAvailable, onRemoveButtonClick }) {
     const [width, setWidth] = useState(0);
+    const [wrapperOnFocus, setWrapperOnFocus] = useState(false);
     const span = useRef();
+    const input = useRef();
 
     useEffect(() => {
         setWidth(span.current.offsetWidth);
@@ -11,9 +14,26 @@ export default function TextFieldSmall({ value, onChange, className }) {
 
 
     return (
-        <div className={className}>
+        <div
+            className={`${wrapperOnFocus ? classes.wrapperOnFocus : classes.wrapperWithoutFocus} ${classes.wrapper} ${className}`}
+            onClick={() => input.current.focus()}
+        >
             <span className={classes.hidden} ref={span}>{value}</span>
-            <input className={classes.input} type="text" value={value} onChange={onChange} style={{width: `${width + 20}px`}} />
+            <div className={classes.inputWrapper}>
+                <input
+                    ref={input}
+                    className={classes.input}
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                    onFocus={() => setWrapperOnFocus(true)}
+                    onBlur={() => setWrapperOnFocus(false)}
+                    style={{ width: `${width}px` }} />
+                {isRemoveButtonAvailable && (value != "") &&
+                    <button className={classes.removeButton} onClick={onRemoveButtonClick}>
+                        <img src={removeIcon} alt="X" />
+                    </button>}
+            </div>
         </div>
     );
 }
