9f2854b738aa8d3a1c2cba909dc2bc6e65b0fd90
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-28 23:19:57 +0300

    Added datePicker

--- src/components/textfield/textfield.jsx
+++ src/components/textfield/textfield.jsx
@@ -1,12 +1,26 @@
+import DatePicker from "react-datepicker";
 import IconButton from "../icon-button/icon-button";
 import classes from "./textfield.module.css";
-import { useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
+import { useRef, useState } from "react";
+import { registerLocale } from  "react-datepicker";
+import { ru } from 'date-fns/locale/ru';
+
+registerLocale('ru', ru);
 
 export default function TextField({ className, mb, title, isRequired, button }) {
     const [value, setValue] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const datePickerRef = useRef(null);
 
     function handleChange(event) {
         setValue(event.target.value);
+    }
+
+    function handleCalendarButtonClick() {
+        if (datePickerRef.current) {
+            datePickerRef.current.setOpen(true);
+        }
     }
 
     return (
@@ -16,9 +30,20 @@ export default function TextField({ className, mb, title, isRequired, button }) 
                 <span className={classes.title}>{title}</span>
             </label>
             <div className={classes.inputContainer}>
-                <input id={title} className={classes.input} type="text" onChange={handleChange} value={value} />
+                {button === "calendar" ?
+                    <DatePicker
+                        id={title}
+                        wrapperClassName={classes.dateInputWrapper}
+                        className={classes.dateInput}
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        ref={datePickerRef}
+                        locale={ru}
+                        dateFormat="dd.MM.yyyy" />
+                    :
+                    <input id={title} className={classes.input} type="text" onChange={handleChange} value={value} />}
                 {button === "search" && <IconButton icon="search" className={classes.button} />}
-                {button === "calendar" && <IconButton icon="calendar" className={classes.button} />}
+                {button === "calendar" && <IconButton icon="calendar" className={classes.button} onClick={handleCalendarButtonClick} />}
             </div>
         </div>
     );
